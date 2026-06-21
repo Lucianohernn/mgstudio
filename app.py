0ce4c837659558e056deee0065aedf6242391968
Lucianohernn <hermannluciano@gmail.com>
2026-06-21 13:20:22 -0300

    Login administrador, cargar cliente bd

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template, request
+from flask import (Flask, render_template, request, redirect)
 
 app = Flask(__name__)
 
@@ -7,8 +7,6 @@ app = Flask(__name__)
 @app.route("/")
 def inicio():
     return render_template("index.html")
-
-
 
 
 @app.route("/admin/login", methods=["GET", "POST"]) #methods indica los parametro(solicitudes http)
@@ -32,11 +30,69 @@ def admin_login(): #funcion
         conexion.close()
 
         if administrador: #tiene dato?
-            return "<h1>Login correcto</h1>"
+            return redirect("/admin/dashboard")
         else:
             return "<h1>Usuario o contraseña incorrectos</h1>"
 
     return render_template("admin_login.html")
+
+
+@app.route("/admin/dashboard")
+def admin_dashboard():
+    return render_template("admin_dashboard.html")
+
+
+@app.route("/admin/clientes")
+def admin_clientes():
+    
+    conexion = sqlite3.connect("mgstudio.db")
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        SELECT id, nombre, dni, telefono
+        FROM clientes
+        ORDER BY nombre
+    """)
+
+    clientes = cursor.fetchall()
+
+    conexion.close()
+
+    return render_template(
+        "admin_clientes.html",
+        clientes=clientes
+    )
+
+@app.route("/admin/clientes/nuevo", methods=["GET", "POST"])
+def nuevo_cliente():
+    
+    if request.method == "POST":
+
+        nombre = request.form["nombre"]
+        dni = request.form["dni"]
+        telefono = request.form["telefono"]
+        fecha_nacimiento = request.form["fecha_nacimiento"]
+
+        conexion = sqlite3.connect("mgstudio.db")
+        cursor = conexion.cursor()
+
+        cursor.execute("""
+            INSERT INTO clientes
+            (nombre, dni, telefono, fecha_nacimiento)
+            VALUES (?, ?, ?, ?)
+        """, (
+            nombre,
+            dni,
+            telefono,
+            fecha_nacimiento
+        ))
+
+        conexion.commit()
+        conexion.close()
+
+        return redirect("/admin/clientes")
+    
+    return render_template("nuevo_cliente.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
